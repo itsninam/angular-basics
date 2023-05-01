40dc6bb701a1a58324ed8bf556243b93882d5938
itsninam <madalinaoancea@hotmail.com>
2023-05-01 18:29:15 -0400

    files and folder structure

---
+++ src/app/angular-files.ts
@@ -0,0 +1,23 @@
+// Angular Files & Folder Structure
+// node modules - contains all 3rd partry libraries that our app depends on, required for development purposes but typically excuded from production
+
+//src - contains all app source code, where we work most
+// all services, modules, components are created in the app folder
+// assets  - contains static assets
+// environment - prodution and development
+
+//index.html - main html file that renders in browser
+
+//main.ts - TS file, entry point of the angular app. First file that is executed when the app runs
+
+//styles.css - global stylesheet
+
+//test.ts - testing environment
+
+//editorconfig - standard coding rules that all devs must follow
+
+//gitignore - files and folder that we want to exclude from git repository
+
+//angular.json - contains all configuration of our angular project
+
+//package.json - contains dependencies which our project is dependend on; devdependencies are required for development purpose only
