d3a87c89bd2c49c96712efee74a1a60ef53a19f8
itsninam <madalinaoancea@hotmail.com>
2023-05-01 18:16:35 -0400

    sett-up angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+//component decorator with 3 properties
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-practice';
+  // the AppComponent class contains two properties with an assigned value: title, message
+  // the value in the html is replaced with the value of this title property
+  title = 'Angular Basics';
+  message =
+    'Angular is a JavaScript framework for building client-side applications';
 }
