c11460907901d9e0ddd8b97565093129afd9518e
itsninam <madalinaoancea@hotmail.com>
2023-05-01 18:36:57 -0400

    typescript

---
+++ src/notes/typescript.ts
@@ -0,0 +1,16 @@
+//Typescript
+// open-source, superset of JS
+// contains some additional features that do not exist in current version of JS
+// strongly typed (JS is dynamically typed), can prevent a lot of bugs and makes app more predictable
+// has some object-oriented features: interface, access modifiers, fields, propertie, generics etc
+//we can catch errors at compile time (instead at run time), allows us to catch errors and fix them before deploying to prod
+
+// declarind variables in JS  - data-type is not specified
+// let firstName = 'John';
+// let age = 28;
+// let isMarried = false;
+
+// declaring variables in TS with data-type
+let firstName: string = 'John';
+let age: number = 28;
+let isMarried: boolean = false;
