a5ca7057afa939f9dbda26337da04679a8a6690d
itsninam <madalinaoancea@hotmail.com>
2023-05-09 18:21:35 -0400

    creating first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container/container.component';
+import { NavComponent } from './Container/nav/nav.component';
+import { HeaderComponent } from './Container/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ContainerComponent, NavComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/notes/component.ts
@@ -0,0 +1,8 @@
+//Component is a TS class decorated with @Component decorator and it contains
+//methods & properties whih we can use in HTML
+//the app component is the main/root component - consists of many nested child components
+
+//Component
+// 1. a class which contains the code required for the view template - contains the UI logic
+// 2. a view templates which defined the UI. It contains the HTML, directives and data binding
+// 3. a decorator which adds meta data to the class, making it a component
