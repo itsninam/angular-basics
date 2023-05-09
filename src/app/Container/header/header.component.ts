9b45af3f2d91d61b19f377366301a64023897b28
itsninam <madalinaoancea@hotmail.com>
2023-05-09 19:01:10 -0400

    data binding

--- src/app/Container/header/header.component.ts
+++ src/app/Container/header/header.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  slogan: string = 'Your one stop shop for everything';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/notes/data-binding.ts
@@ -0,0 +1,11 @@
+//What is Data Binding?
+//allows us to communicate between a component class and its corresponding view template
+//in the component class we specify the UI logic - properties and methods which we can use
+//in the view template of that component
+
+//one-way data binding
+// from component class to view template or vice versa
+
+//two-way data binding
+//from component class to view template and view template to component class
+// requires a combination of property binding and event binding
